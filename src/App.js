e4f7ea877249ce1dd63b5911c1775a716e5cc442
varunsingh09 <singh.varun1985@gmail.com>
2023-11-06 22:51:43 +0530

    heroku react app

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+        <h2>Welcome to Heroku React App</h2>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
